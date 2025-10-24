2cb9481e710b54a5870d07f2e4e0027fa76abc6c
Bjørn Moholt <bjorn@mewb.no>
2025-10-24 15:26:10 +0200

    Picks Physical Device

--- src/main.zig
+++ src/main.zig
@@ -61,7 +61,10 @@ pub fn main() !void {
     const physicalDevices = try allocator.alloc(c.VkPhysicalDevice, deviceCount);
     _ = c.vkEnumeratePhysicalDevices(instance, &deviceCount, physicalDevices.ptr);
     std.log.info("{}", .{deviceCount});
-    for (physicalDevices) |device| {
+
+    var physicalDeviceIndex: ?usize = null;
+
+    for (physicalDevices, 0..) |device, deviceIndex| {
         var properties: c.VkPhysicalDeviceProperties = undefined;
         c.vkGetPhysicalDeviceProperties(device, &properties);
         std.log.info("{s}", .{properties.deviceName});
@@ -70,7 +73,54 @@ pub fn main() !void {
         _ = c.vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, null);
         const queueFamilies = try allocator.alloc(c.VkQueueFamilyProperties, queueFamilyCount);
         _ = c.vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.ptr);
+
+        // Get queue families
+        var graphicsFamily: ?usize = null;
+        var presentFamily: ?usize = null;
+        for (queueFamilies, 0..) |family, i| {
+            if (family.queueFlags & c.VK_QUEUE_GRAPHICS_BIT != 0) {
+                graphicsFamily = i;
+            }
+
+            var presentSupport: c.VkBool32 = c.VK_FALSE;
+            _ = c.vkGetPhysicalDeviceSurfaceSupportKHR(device, @intCast(i), window.surface, &presentSupport);
+            if (presentSupport == c.VK_TRUE) {
+                presentFamily = i;
+            }
+        }
+
+        if (graphicsFamily == null or presentFamily == null) continue;
+
+        var extensionCount: u32 = 0;
+        _ = c.vkEnumerateDeviceExtensionProperties(device, null, &extensionCount, null);
+
+        const availableExtensions = try allocator.alloc(c.VkExtensionProperties, extensionCount);
+        _ = c.vkEnumerateDeviceExtensionProperties(device, null, &extensionCount, availableExtensions.ptr);
+
+        var hasSwapchain = false;
+        for (availableExtensions) |ext| {
+            const name = @as([*:0]const u8, @ptrCast(&ext.extensionName));
+            if (std.mem.eql(u8, std.mem.span(name), c.VK_KHR_SWAPCHAIN_EXTENSION_NAME)) {
+                hasSwapchain = true;
+                break;
+            }
+        }
+
+        if (!hasSwapchain) continue;
+
+        var formatCount: u32 = 0;
+        _ = c.vkGetPhysicalDeviceSurfaceFormatsKHR(device, window.surface, &formatCount, null);
+        var presentModeCount: u32 = 0;
+        _ = c.vkGetPhysicalDeviceSurfacePresentModesKHR(device, window.surface, &presentModeCount, null);
+
+        if (formatCount == 0 or presentModeCount == 0) continue;
+
+        physicalDeviceIndex = deviceIndex;
+        break;
     }
+
+    if (physicalDeviceIndex == null) return;
+    std.log.info("Chosen device: {?}", .{physicalDeviceIndex});
 
     // Event loop
     while (windows.Window.pollEvents()) {
