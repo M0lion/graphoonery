4185f33175cb80a3808fe70c9621c0641107e6d6
Bjørn Moholt <bjorn@mewb.no>
2025-11-24 12:13:01 +0100

    Fixes some wayland glfw interaction stuff

--- window/src/lib.zig
+++ window/src/lib.zig
@@ -14,15 +14,25 @@ pub fn init() !void {
         @panic("Failed to init glfw");
     }
 
-    const window = c.glfwCreateWindow(500, 500, "Test", null, null) orelse
+    c.glfwWindowHint(c.GLFW_CLIENT_API, c.GLFW_NO_API);
+
+    var width: c_int = 500;
+    var height: c_int = 500;
+
+    const window = c.glfwCreateWindow(width, height, "Test", null, null) orelse
         @panic("Could not create window");
 
-    const display = c.glfwGetWaylandDisplay();
-    const surface = c.glfwGetWaylandWindow(window);
+    c.glfwPollEvents();
+
+    const display = c.glfwGetWaylandDisplay() orelse @panic("Could not get display");
+    const surface = c.glfwGetWaylandWindow(window) orelse @panic("Could not get surface");
+
+    c.glfwGetWindowSize(window, &width, &height);
+
     var context = try vulkan.context.VulkanContext.init(.{
         .display = @ptrCast(display),
         .surface = @ptrCast(surface),
-    }, 500, 500, allocator);
+    }, @intCast(width), @intCast(height), allocator);
 
     while (c.glfwWindowShouldClose(window) == c.GLFW_FALSE) {
         c.glfwPollEvents();
